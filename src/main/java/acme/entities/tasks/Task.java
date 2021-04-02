8a0606323f9768a70833b5b712ba75b4114cdd68
manmunesp12 <manmunesp1@alum.us.es>
2021-04-03 00:10:00 +0200

    Añadido algunas tareas a la base de datos

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -3,6 +3,7 @@ package acme.entities.tasks;
 import java.beans.Transient;
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -11,7 +12,12 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Task extends DomainEntity{
 	
 	// Serialisation identifier
