fd9fe83fd89cabaa2874b845cb9edf0483623a86
manmunesp12 <manmunesp1@alum.us.es>
2021-04-02 22:41:34 +0200

    Primera versión de las entidades de los requisitos de información.

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,61 @@
+package acme.entities.tasks;
+
+import java.beans.Transient;
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+
+public class Task extends DomainEntity{
+	
+	// Serialisation identifier
+	
+	protected static final long serialVersionUID = 1L;
+		
+	// Attributes
+	
+	@NotBlank
+	@Length(max=80)
+	protected String title;
+	
+	@Future
+	@NotNull
+	protected Date	begin;
+
+	@Future
+	@NotNull
+	protected Date end;
+	
+	@NotBlank
+	@Length(min=500)
+	protected String description;
+	
+	@URL 
+	protected String link;
+	
+	@NotNull
+    protected Boolean isPublic;
+	
+	//	Derived attributes
+	@Transient
+	public Double  getWorkload(){
+		return (double) (this.end.getTime() - this.begin.getTime()) / (1000 * 3600) ;
+	}
+	
+	 @Transient
+	 public Boolean isFinished() {
+	    Date now;
+	    now=new Date();
+	    return now.after(this.end);
+	}
+	
+	
+	//	Relationships
+
+}
