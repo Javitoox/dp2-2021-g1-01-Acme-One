eb0cf29aa57f3f95c243ef0097b0e87c9345b55a
manmunesp12 <manmunesp1@alum.us.es>
2021-04-20 19:26:59 +0200

    Modificada una consulta

--- src/main/java/acme/features/administrator/taskDashboard/AdministratorTaskDashboardRepository.java
+++ src/main/java/acme/features/administrator/taskDashboard/AdministratorTaskDashboardRepository.java
@@ -22,7 +22,7 @@ public interface AdministratorTaskDashboardRepository extends AbstractRepository
 	@Query("select count(t) from Task t where ?1 <= t.end")
 	Integer totalNumberOfPendingTasks(Date now);
 	
-	@Query("select count(t.getWorkload) from Task t")
+	@Query("select count(t.workload) from Task t")
 	Double averageNumberOfWorkload();
 
 }
