3622e26381c7111fb151fd4aa651c04c2329866e
mrb <62081471+maribelrb22@users.noreply.github.com>
2021-04-25 09:16:45 +0200

    Merge branch 'Task-036'

--- src/main/java/acme/entities/workPlan/WorkPlan.java
+++ src/main/java/acme/entities/workPlan/WorkPlan.java
@@ -10,20 +10,13 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import acme.entities.tasks.Task;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.beans.Transient;
-import java.util.Collection;
-import java.util.Date;
 
 @Entity
 @Getter
