26acfafc928bbb3945957298e2fd9c8a065c256a
manmunesp12 <manmunesp1@alum.us.es>
2021-04-26 00:16:37 +0200

    Fix bug in create, update and publish task for manager

--- src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
+++ src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
@@ -81,8 +81,6 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		final Date now =new Date();
 		final Date begin = entity.getBegin();
 		final Date end = entity.getEnd();
-		entity.setExecutionPeriod();
-		final double periodo = entity.getExecutionPeriod(); 
 		
 		final boolean titleSpam = this.spam.isItSpam(entity.getTitle());
 		final boolean descripcionSpam = this.spam.isItSpam(entity.getDescription());
@@ -97,9 +95,13 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		if(!errors.hasErrors("end")) {
 			errors.state(request, end.after(now), "end", "manager.task.form.error.must-be-in-future");
 		}
-		if(!errors.hasErrors("workload")) {
+		if(!errors.hasErrors("begin")&&!errors.hasErrors("end")) {
+			entity.setExecutionPeriod();
+			final double periodo = entity.getExecutionPeriod(); 
+			if(!errors.hasErrors("workload")) {
 			errors.state(request, periodo>entity.getWorkload(), "workload", "manager.task.form.error.must-be-less-than-work-period");
 			errors.state(request, periodo>entity.getWorkload(), "workload", "("+periodo+")");
+			}
 		}
 		final int ent = (int) entity.getWorkload();
 		final double dec = entity.getWorkload() - ent;
